58d6fee980b693f8ee45a03e2573f815d758791a
Larissa <dejamepienso@gmail.com>
2024-08-21 18:55:04 -0600

    Update about view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,42 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<!-- @format -->
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<template>
+	<div class="flex flex-col items-center w-full mb-[120px] md:mb-0">
+		<h1 class="mt-[56px] text-[44px] text-[#E5E5E5] text-center font-semibold">
+			About Me
+		</h1>
+
+		<div
+			class="flex items-center md:items-stretch bg-[#FFE194] flex-col sm:flex-row w-full max-w-[1124px] h-auto mt-[64px] md:mt-[108px] rounded-bl-[122px] rounded-tr-[122px] relative">
+			<p
+				class="big-txt text-[88px] sm:text-[120px] absolute top-[-100px] sm:top-[-140px] left-auto text-center w-full z-0 opacity-5 font-bold text-[#FFE194]">
+				About Me
+			</p>
+
+			<div
+				class="flex w-auto h-[200px] md:h-auto sm:w-1/3 bg-transparent md:bg-[#FFE194] rounded-bl-[122px] relative z-[2]">
+				<div
+					class="rounded-full md:rounded-bl-[122px] md:rounded-br-none md:rounded-tr-none md:rounded-tl-none overflow-hidden w-[200px] h-[200px] sm:w-[396px] sm:h-auto bg-cover bg-no-repeat bg-right mt-[24px] md:mt-0"
+					style="background-image: url(../../public/about_me.jpg)"></div>
+			</div>
+
+			<div
+				class="flex flex-col w-2/3 bg-transparent md:bg-[#FFE194] text-[#373737] rounded-tr-[122px] px-[24px] md:px-[48px] py-[48px] relative z-[2]">
+				<h2
+					class="text-center md:text-left font-semibold md:text-[32px] text-[24px]">
+					Larissa Avila
+				</h2>
+
+				<p
+					class="text-center md:text-left text-[14px] md:text-[20px] mt-[24px] md:mt-[64px]">
+					I'm a Frontend Developer with UI Design background and strong skills
+					implementing HTML & CSS in different environments like Vue, React,
+					Angular, etc. I have always enjoyed laying out and all related with
+					user interactions. I think one of the skills that makes me different,
+					in addition to my wide CSS knowledge, is to know all about user
+					interfaces, design patterns and development interactions.
+				</p>
+			</div>
+		</div>
+	</div>
+</template>
