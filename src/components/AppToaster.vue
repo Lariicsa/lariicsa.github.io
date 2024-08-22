7b103273c0a42ebe4bb2a5de063aa3b8a653f161
Larissa <dejamepienso@gmail.com>
2024-08-21 19:06:23 -0600

    update email

--- src/components/AppToaster.vue
+++ src/components/AppToaster.vue
@@ -3,7 +3,6 @@
 <script setup>
 	import { ref, defineProps } from "vue";
 
-
 	const props = defineProps({
 		show: {
 			type: Boolean,
@@ -11,13 +10,13 @@
 		},
 	});
 
-  const isShown = ref(props.show)
+	const isShown = ref(props.show);
 </script>
 <template>
 	<Transition :duration="550" name="nested">
 		<div
 			v-if="props.show"
-			class="flex  w-[156px] h-[48px] justify-center items-center bg-[#C3D994] text-[16px] font-medium text-[#373737] rounded-md shadow-md">
+			class="flex w-[156px] h-[48px] justify-center items-center bg-[#C3D994] text-[16px] font-medium text-[#373737] rounded-md shadow-md absolute bottom-[80px] left-auto">
 			Copied e-Mail
 		</div>
 	</Transition>
