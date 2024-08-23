a9f91407190b4a208f78cc90ae685fd919879a50
Larissa <dejamepienso@gmail.com>
2024-08-23 15:58:42 -0600

    Update jobcards

--- src/components/AppTag.vue
+++ src/components/AppTag.vue
@@ -2,7 +2,7 @@
 <script setup></script>
 <template>
 	<div
-		class="w-auto mr-[12px] mt-[8px] sm:mt-[16px] flex justify-center items-centere bg-[#d6e7fa] text-[#2d2e3b] text-[10px] sm:text-[14px] rounded-full py-[4px] px-[8px] sm:px-[12px]">
+		class="w-auto mr-[12px] mt-[8px] sm:mt-[16px] flex justify-center items-centere bg-[#d6e7fa] text-[#2d2e3b] text-[10px] sm:text-[12px] rounded-full py-[4px] px-[8px] sm:px-[12px]">
 		<slot></slot>
 	</div>
 </template>
--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -1,80 +1,13 @@
 <!-- @format -->
 
 <script setup>
-import AppTimeline from "@/components/AppTimeline.vue";
+	import AppTimeline from "@/components/AppTimeline.vue";
 	import JobCard from "@/components/JobCard.vue";
+	import experience from '@/data/experience'
 
-	const experiences = [
-		{
-			title: "Sr. Frontend Developer",
-			subtitle: "Latouraine Inc.",
-			date: 'current',
-			abstract: `Develop Web Apps (CMS, Portal, Social) and Landing pages for Adult web content.
-			Fix UI and implement new FE technology.`,
-			skills: [
-				"Vue",
-				"Pinia",
-				"Tailwind",
-				"Vanilla Js",
-				"SCSS",
-				"jQuery",
-				"PHP",
-				"Laravel",
-				"Inertia",
-				"Vagrant",
-				"Gitflow",
-				"Scrum",
-				"Envoyer",
-			],
-		},
+	const experiences = experience
+	
 
-
-		{
-			title: "Sr. Frontend Developer",
-			subtitle: "Latouraine Inc.",
-			date: '2020 - 2022',
-			abstract: `Develop Web Apps (CMS, Portal, Social) and Landing pages for Adult web content.
-			Fix UI and implement new FE technology.`,
-			skills: [
-				"Vue",
-				"Pinia",
-				"Tailwind",
-				"Vanilla Js",
-				"SCSS",
-				"jQuery",
-				"PHP",
-				"Laravel",
-				"Inertia",
-				"Vagrant",
-				"Gitflow",
-				"Scrum",
-				"Envoyer",
-			],
-		},
-
-		{
-			title: "Sr. Frontend Developer",
-			subtitle: "Latouraine Inc.",
-			date: '2020 - 2022',
-			abstract: `Develop Web Apps (CMS, Portal, Social) and Landing pages for Adult web content.
-			Fix UI and implement new FE technology.`,
-			skills: [
-				"Vue",
-				"Pinia",
-				"Tailwind",
-				"Vanilla Js",
-				"SCSS",
-				"jQuery",
-				"PHP",
-				"Laravel",
-				"Inertia",
-				"Vagrant",
-				"Gitflow",
-				"Scrum",
-				"Envoyer",
-			],
-		},
-	];
 </script>
 <template>
 	<div class="flex mx-auto flex-col">
@@ -82,10 +15,10 @@ import AppTimeline from "@/components/AppTimeline.vue";
 			Experience
 		</h1>
 
-		<div class="flex justify-between h-auto w-full max-w-[1230px] mt-[84px] sm:mt-[64px] mb-[100px]">
-			<AppTimeline/>
+		<div
+			class="flex justify-between h-auto w-full max-w-[1230px] mt-[84px] sm:mt-[64px] mb-[100px]">
+			<AppTimeline />
 			<div class="flex flex-col w-full">
-				
 				<JobCard :items="experiences" />
 			</div>
 		</div>
